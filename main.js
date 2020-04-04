463850948c7be2c7c94c9fe9c098b2ba27f4b162
Giacomo <kirtash.k@live.it>
2020-04-04 17:26:59 +0200

    Esercizio BONUS Completo

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-// Creare in JAVASCRIPT una griglia formata da 8x8 rettangolini tutti bianchi.
+// Creare in JAVASCRIPT una griglia formata da 8x8 rettangolini tutti vuoti.
 // 15 di questi rettangolini (scelti a caso dal programma) se cliccati mostrano dei fiori, gli altri saranno il prato che li circonda. In alto alla griglia creata ci sono i contatori col numero di caselle con fiori cliccate e il numero di caselle prato cliccate.
 // In fondo ci sono 2 bottoni: uno mostra termina il gioco mostrando la posizione di tutto, l'altro la mostra e poi resetta la griglia e il punteggio
 
@@ -55,7 +55,7 @@ function posizionaFiore () {
   for(var i=0; i<15; i++){
     // eq() Reduce the set of matched elements to the one at the specified index.
     //in soldoni: prendimi l'elemento contenuto in arrayIndici che ha indice i
-    // e aggiungici la classe fiore
+    // e aggiungi la classe fiore all'elemento children di appoggioContainer corrispondente
     appoggoContainer.children().eq(arrayIndici[i]).addClass("fiore");
   }
 }
